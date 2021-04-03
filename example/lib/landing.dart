9627e1afb455e5f81a83648e42c264b574e6adf5
Febrian90s <febrian90s@example.com>
2021-04-03 20:55:09 +0800

    membuat body dengan teks center LOGIN

--- example/lib/landing.dart
+++ example/lib/landing.dart
@@ -9,14 +9,18 @@ class HalamanSatu extends StatelessWidget{
   @override
 
   Widget build(BuildContext context){
+    var actions;
     return new Scaffold(
       appBar : new AppBar(
-        backgroundColor: Colors.green[200],
+        backgroundColor: Colors.green[800],
         leading : new Icon(Icons.home),
-        title : new Center(child : new Text ('Klik Tani')),
+        title : new Center(child : new Text ('Klik TANI')),
         actions: <Widget>[
-          new Icon(Icons.search)
+          new Icon(Icons.camera)
         ],
+      ),
+      body: new Container(
+        child: new Center (child: new Text ('LOGIN')),
       ),
     );
   }
